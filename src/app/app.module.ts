f7defd730c4063fa77605fbb0db36f0818762d2f
developermsd7 <developermsd7@gmail.com>
2022-12-05 23:52:25 +0530

    added logo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppComponent } from './app.component';
 import { CustomerModelComponent } from './customer-model/customer-model.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomerModelComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CustomerModelComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
