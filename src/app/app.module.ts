5abfd9d97376db2a209183eb36bf98712d5c2213
developermsd7 <developermsd7@gmail.com>
2022-12-05 22:59:35 +0530

    adding services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerModelComponent } from './customer-model/customer-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerModelComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customer-model/customer-model.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-model',
+  templateUrl: './customer-model.component.html',
+  styleUrls: ['./customer-model.component.css']
+})
+export class CustomerModelComponent {
+
+}
